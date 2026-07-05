ae8f3e2e21cab9965cbed640d843d2bf8724b2c9
dvenkat <dhruv.venkat.2024@gmail.com>
2026-07-05 17:22:18 -0600

    added flow for reading from stdin and done wgrep

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -4,15 +4,25 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	char *searchTerm = argv[1];
-	FILE *fptr;
-
 	if (argc == 1) {
 		printf("wgrep: searchterm [file ...]\n");
 		return 1;
 	}
 
+	char *searchTerm = argv[1];
+	FILE *fptr;
+
 	// TODO: create a flow for grepping from standard input
+	if (argc == 2) { // if no filename is given, read from stdin
+		char line[256];
+		while (fgets(line, sizeof(line), stdin)) {
+			if (strstr(line, searchTerm)) {
+				printf("%s", line);
+			}
+		}
+		
+	}
+	
 	//printf("search term: %s\n", searchTerm);
 
 	// iterate thru each file and look for the search term
